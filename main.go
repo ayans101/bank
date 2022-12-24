160e8fda45a1154c3952e245c30178f9b0a29b3a
Ayan Sarkar <ayansarkar367@gmail.com>
2022-12-24 12:07:21 +0530

    load config from file & environment variables

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/ayans101/bank/api"
 	db "github.com/ayans101/bank/db/sqlc"
+	"github.com/ayans101/bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
