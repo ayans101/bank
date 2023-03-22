2149948891936f037f286545521f3a16b1a0a0be
Ayan Sarkar <ayansarkar367@gmail.com>
2023-03-22 22:02:10 +0530

    implement login user api

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,26 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/ayans101/bank/db/sqlc"
+	"github.com/ayans101/bank/util"
 	"github.com/gin-gonic/gin"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	if err != nil {
+		return nil
+	}
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
